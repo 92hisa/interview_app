87e8ff91d051b4d544cfda9eb4bdf3dea8134998
92hisa <73869235+92hisa@users.noreply.github.com>
2021-03-29 16:50:51 +0900

    Added column of profile_image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_033426) do
+ActiveRecord::Schema.define(version: 2021_03_29_074652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_033426) do
     t.string "name", null: false
     t.integer "gender", default: 0, null: false
     t.text "about_me"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
