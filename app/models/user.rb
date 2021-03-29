428f877319e0026a7b5360974c24c84a4bf88831
92hisa <73869235+92hisa@users.noreply.github.com>
2021-03-29 11:11:43 +0900

    displayed about_me column on registrations/edit.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :name, presence: true
+  validates :about_me, length: { maximum: 500 }
 
   enum gender: { man: 0, woman: 1 }
 end
