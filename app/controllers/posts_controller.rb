9220df8e31a749a0cf2c88a67f5a8b93094e6970
92hisa <73869235+92hisa@users.noreply.github.com>
2021-03-30 13:36:38 +0900

    created  posts/show page as temporary

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :email, :fee, :due_date, :experience, :detail).merge(user_id: current_user.id)
