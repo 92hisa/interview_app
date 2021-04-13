67b67a8f1b0e898bf57c1c7432586d16e025bdbd
92hisa <73869235+92hisa@users.noreply.github.com>
2021-04-13 14:37:54 +0900

    edited video-end function

--- app/javascript/packs/room.js
+++ app/javascript/packs/room.js
@@ -1,22 +1,6 @@
-// $(document).on('turbolinks:load', function() {
-
-  function down(){
-    const videoEnd = document.getElementById("video-end")
-    // if (videoEnd.getAttribute("data-load") != null) {
-    //   return null;
-    // }
-    // videoEnd.setAttribute("data-load", "true");
-    videoEnd.addEventListener('click', () => {
-    location.reload();
-    // videoEnd.removeAttribute("data-load");
-  })
-  }
-  setInterval(down, 500)
-
   import Vue from 'vue/dist/vue.esm'
   import Room from '../room.vue'
   
-  // document.addEventListener('DOMContentLoaded', () => {
     function loading(){
   
   
@@ -27,5 +11,5 @@
       components: { Room }
     })
   }
-  // })
   setTimeout(loading,   500)
+
