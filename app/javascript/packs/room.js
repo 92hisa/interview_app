cb26619fc78aba422a907daefaa06109eee30a26
92hisa <73869235+92hisa@users.noreply.github.com>
2021-04-13 11:01:35 +0900

    added the button to stop video

--- app/javascript/packs/room.js
+++ app/javascript/packs/room.js
@@ -1,5 +1,18 @@
 // $(document).on('turbolinks:load', function() {
-  
+
+  function down(){
+    const videoEnd = document.getElementById("video-end")
+    // if (videoEnd.getAttribute("data-load") != null) {
+    //   return null;
+    // }
+    // videoEnd.setAttribute("data-load", "true");
+    videoEnd.addEventListener('click', () => {
+    location.reload();
+    // videoEnd.removeAttribute("data-load");
+  })
+  }
+  setInterval(down, 500)
+
   import Vue from 'vue/dist/vue.esm'
   import Room from '../room.vue'
   
