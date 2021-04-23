7af7b1bfac0c79283a1ff2b3c8ec598bcf4c5461
92hisa <73869235+92hisa@users.noreply.github.com>
2021-04-24 02:24:32 +0900

    added chat sidebar on rooms/show page

--- app/javascript/room.vue
+++ app/javascript/room.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
+<div id="app">
   <div class="lead-text text-center">
     <span>① マイクとカメラを設定 (選択すると自動でwebカメラが起動します。)</span>
   </div>
 
-  <div class="option-area mb-3 text-center">
-  マイク:
-    <select v-model="selectedAudio" @change="onChange">
-      <option disabled value="">Please select one</option>
-      <option v-for="(audio, key, index) in audios" v-bind:key="index" :value="audio.value">
-        {{ audio.text }}
-      </option>
-    </select>
+  <div class="mb-3 d-flex align-items-center justify-content-between">
+    <span class="camera-option mb-3 text-center">
+    マイク:
+      <select v-model="selectedAudio" @change="onChange">
+        <option disabled value="">Please select one</option>
+        <option v-for="(audio, key, index) in audios" v-bind:key="index" :value="audio.value">
+          {{ audio.text }}
+        </option>
+      </select>
+    </span>
 
     <span class="camera-option mb-3 text-center">
     カメラ:
@@ -24,29 +26,30 @@
     </span>
   </div>
 
-    <div class="card">
-      <div class="d-flex align-items-center justify-content-between">
-        <video id="their-video" width="781" autoplay playsinline></video>
-        <div class="card-body border p-1" style="width: auto;">
-          <video id="my-video" muted="true" width="202" autoplay playsinline></video>
-        </div>
+
+  <div class="card">
+    <div class="d-flex align-items-center justify-content-between">
+      <video id="their-video" width="781" autoplay playsinline></video>
+      <div class="card-body border p-1" style="width: auto;">
+        <video id="my-video" muted="true" width="202" autoplay playsinline></video>
       </div>
     </div>
-    <p class="mt-3" style="font-size: 20px;">
-      Your Peer ID: <span id="my-id">{{ peerId }}</span>
-    </p>
-
-    <div class="lead-text text-center">
-      <span>② 相手のPeer IDを入力して、 通話開始をクリック(しばらくすると、ビデオ通話が開始します。)</span>
-    </div>
-
-    <div class="call-area text-center">
-      <input v-model="calltoid" placeholder="相手のIDを入力してください" class="video-chat-form"/>
-      <!-- <div class="search-btn"> -->
-      <button @click="makeCall" class="btn btn-success btn-sm ml-3">通話開始</button>
-      <input type="button" value="通話終了" class="btn btn-outline-danger btn-sm ml-2" onclick="window.location.reload();" />
-    </div>
   </div>
+  <p class="mt-3" style="font-size: 20px;">
+    あなたのPeer ID: <span id="my-id">{{ peerId }}</span>
+  </p>
+
+  <div class="lead-text text-center">
+    <span>② お相手のPeer IDを入力して、 通話開始をクリック(しばらくすると、ビデオ通話が開始します。)</span>
+  </div>
+
+  <div class="call-area text-center">
+    <input v-model="calltoid" placeholder="相手のIDを入力してください" class="video-chat-form"/>
+    <!-- <div class="search-btn"> -->
+    <button @click="makeCall" class="btn btn-success btn-sm ml-3">通話開始</button>
+    <input type="button" value="通話終了" class="btn btn-outline-danger btn-sm ml-2" onclick="window.location.reload();" />
+  </div>
+</div>
 </template>
 
 <script>
