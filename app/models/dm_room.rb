4f1232eae737a9c49065eb0fc0f0355389d05b5e
92hisa <73869235+92hisa@users.noreply.github.com>
2021-04-28 22:48:00 +0900

    chaged the photo in the first view

--- app/models/dm_room.rb
+++ app/models/dm_room.rb
@@ -2,6 +2,7 @@ class DmRoom < ApplicationRecord
   has_many :dms, dependent: :destroy
   has_many :entries, dependent: :destroy
   has_many :notifications, dependent: :destroy
+  belongs_to :user, dependent: :destroy
 
   validates :user_id, presence: true
 end
